05c80523b0b455b6a365e198510bd2e182d7d33c
Brian Lara <brian.lara@outlook.com.ar>
2023-03-13 18:03:04 -0300

    Clase 01

---
+++ Clase 01/calculos.js
@@ -0,0 +1,10 @@
+// var numero1 = 0; 
+
+let numero = 0; // variable mutable
+const numero2 = 0; // variable inmutable
+
+numero = 7;
+
+numero2 = 7; // Error
+
+console.log("El valor de la variable es: " + numero);
