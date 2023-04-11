efedbb94f0e9af95d8c18e109466db8095bc221f
Brian Lara <brian.lara@outlook.com.ar>
2023-04-11 18:06:58 -0300

    Clase 10

---
+++ Clase 10/controllers/products.controllers.js
@@ -0,0 +1,131 @@
+import * as services from '../services/products.services.js'
+import * as views from '../views/products.views.js'
+
+
+function getProducts(req, res) {
+
+    services.getProducts({ deleted: true })
+        .then(function (products) {
+            res.send(views.createProductListPage(products))
+        })
+}
+
+
+function getProductDetail(req, res) {
+    const idProduct = req.params.idProduct
+
+    services.getProductById(idProduct)
+        .then(function (product) {
+            if (product) {
+                res.send(views.createProductPage(product))
+            }
+            else {
+                res.send(views.createPage('Producto no encontrado', '<p>El producto no existe</p>'))
+            }
+
+        })
+}
+
+function createNewProductPage(req, res) {
+    res.send(views.createNewProductPage())
+}
+
+function createProduct(req, res) {
+    const product = {
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description
+    }
+
+    services.createProduct(product)
+        .then(function (newProduct) {
+            res.send(views.createPage('Producto creado', `<p>El producto ${newProduct.name} ha sido creado con el id ${newProduct.id}</p>`))
+        })
+        .catch(function (error) {
+            res.send(views.createPage('Error creando producto', `<p>Se ha producido un error creando el producto</p>`))
+        })
+}
+
+
+function editProductPage(req, res) {
+    const id = req.params.idProduct
+
+    services.getProductById(id)
+        .then(function (product) {
+            if (product) {
+                res.send(views.formEditProduct(product))
+            }
+            else {
+                res.send(views.createPage('Producto no encontrado', '<p>El producto no existe</p>'))
+            }
+        })
+
+}
+
+
+function editProduct(req, res) {
+    const id = parseInt(req.params.idProduct)
+    const product = {
+        name: req.body.name,
+        price: parseInt(req.body.price),
+        description: req.body.description
+    }
+
+    services.editProduct(id, product)
+        .then(function (product) {
+            if (product) {
+                res.send(views.createPage('Producto modificado', `<p>El producto ${product.name} ha sido modificado</p>`))
+            }
+            else {
+                res.send(views.createPage('Producto no encontrado', '<p>El producto no existe</p>'))
+            }
+        })
+        .catch(function (error) {
+            res.send(views.createPage('Error modificando producto', `<p>Se ha producido un error modificando el producto</p>`))
+        })
+}
+
+
+
+function deleteProductPage(req, res) {
+    const id = req.params.idProduct
+
+    services.getProductById(id)
+        .then(function (product) {
+            if (product) {
+                res.send(views.formDeleteProduct(product))
+            }
+            else {
+                res.send(views.createPage('Producto no encontrado', '<p>El producto no existe</p>'))
+            }
+        })
+
+}
+
+function deleteProduct(req, res) {
+    const id = parseInt(req.params.idProduct)
+
+    services.deleteProduct(id)
+        .then(function (product) {
+            if (product) {
+                res.send(views.createPage('Producto eliminado', `<p>El producto ${product.name} ha sido eliminado</p>`))
+            }
+            else {
+                res.send(views.createPage('Producto no encontrado', '<p>El producto no existe</p>'))
+            }
+        })
+        .catch(function (error) {
+            res.send(views.createPage('Error eliminando producto', `<p>Se ha producido un error eliminando el producto</p>`))
+        })
+}
+
+export {
+    getProducts,
+    getProductDetail,
+    createNewProductPage,
+    createProduct,
+    editProductPage,
+    editProduct,
+    deleteProductPage,
+    deleteProduct
+}
---
+++ Clase 10/services/products.services.js
@@ -0,0 +1,110 @@
+import { readFile, writeFile } from 'node:fs/promises'
+
+
+async function getProducts(filter = {}) {
+    return readFile('./data/products.json')
+        .then(function (data) {
+            return JSON.parse(data) // convierte el string a objeto
+        })
+        .then(function (products) {
+            if (filter.deleted) {
+                const productsFiltered = []
+
+                for (let i = 0; i < products.length; i++) {
+                    if (!products[i].deleted) {
+                        productsFiltered.push(products[i])
+                    }
+                }
+
+                return productsFiltered
+            }
+            else {
+                return products
+            }
+        })
+        .catch(function () {
+            return []
+        })
+}
+
+async function getProductById(idProduct) {
+    return getProducts()
+        .then(function (products) {
+            let product = null
+            for (let i = 0; i < products.length; i++) {
+                if (products[i].id == idProduct) {
+                    product = products[i]
+                    break // corta cualquier ciclo repetitivo 
+                }
+            }
+
+            return product
+        })
+}
+
+async function createProduct(product) {
+    const products = await getProducts()
+
+    const newProduct = {
+        ...product,
+        id: products.length + 1,
+    }
+
+    products.push(newProduct)
+
+    await writeFile('./data/products.json', JSON.stringify(products))
+
+    return newProduct
+}
+
+async function editProduct(idProduct, product) {
+    const products = await getProducts()
+    let editProduct = null
+
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id == idProduct) {
+            products[i] = {
+                ...product, // spread operator
+                id: products[i].id,
+            }
+            editProduct = products[i]
+            break
+        }
+    }
+
+    if (editProduct) {
+        await writeFile('./data/products.json', JSON.stringify(products))
+    }
+
+    return editProduct
+}
+
+
+async function deleteProduct(idProduct) {
+    const products = await getProducts()
+    let editProduct = null
+
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id == idProduct) {
+            products[i].deleted = true
+            editProduct = products[i]
+            break
+        }
+    }
+
+    if (editProduct) {
+        await writeFile('./data/products.json', JSON.stringify(products))
+    }
+
+    return editProduct
+
+
+}
+
+export {
+    getProducts,
+    getProductById,
+    createProduct,
+    editProduct,
+    deleteProduct
+}
